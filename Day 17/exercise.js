9bade6b6ab50ff91d27a480bcc26234674d4c8e6
Shivanshu <ks7876555@gmail.com>
2023-11-03 10:38:39 +0530

    Do Web Storage

---
+++ Day 17/exercise.js
@@ -0,0 +1,9 @@
+
+//Store you first name, last name, age, country, city in your browser localStorage.
+
+localStorage.setItem('name', 'Shivanshu Koundal');
+localStorage.setItem('age', 19);
+localStorage.setItem('country', 'india');
+localStorage.setItem('city', 'kangra');
+console.log(localStorage);
+// use console to execute this block of code
