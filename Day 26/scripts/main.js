7a5bb187c55dcb78954f3606bec6a916a7965308
Shivanshu <ks7876555@gmail.com>
2023-11-23 00:02:52 +0530

    first commit

---
+++ Day 26/scripts/main.js
@@ -0,0 +1,19 @@
+// btns
+const startWordBtn = document.querySelector('#stWordBtn')
+const anyWordBtn = document.querySelector('#anyWordBtn')
+const sortBtn = document.querySelector('#sortBtn')
+const searchField = document.querySelector('#searchInput')
+const searchBtn = document.querySelector('#searchBtn')
+
+startWordBtn.addEventListener('click',{
+
+})
+anyWordBtn.addEventListener('click',{
+    
+})
+sortBtn.addEventListener('click',{
+    
+})
+searchBtn.addEventListener('click',{
+    
+})
