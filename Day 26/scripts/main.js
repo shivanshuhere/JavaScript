fd421c415ba66ae687261fc585cb86788642a5c4
Shivanshu <ks7876555@gmail.com>
2023-11-25 13:55:57 +0530

    Add search and sort fxn

--- Day 26/scripts/main.js
+++ Day 26/scripts/main.js
@@ -4,6 +4,40 @@ const anyWordBtn = document.querySelector('#anyWordBtn')
 const sortBtn = document.querySelector('#sortBtn')
 const searchField = document.querySelector('#searchInput')
 const searchBtn = document.querySelector('#searchBtn')
+const lowerCountries = countries.map(ele => ele.toLowerCase())
+
+//  wraper 
+const wrap = document.getElementById('wrapper')
+
+let reqArr = [] // required countreis
+searchField.addEventListener('input', e=>{
+    let input = ''; input = e.target.value.toLowerCase() // input string
+    reqArr = []
+    // search with any word
+    // lowerCountries.forEach(ele=> {
+    //     if (ele.includes(input)) {
+    //     reqArr.push(ele.toUpperCase())
+    //     }
+    // })
+
+    // search with starting word
+    lowerCountries.forEach(ele=> {
+        if (ele.startsWith(input)) {
+        reqArr.push(ele.toUpperCase())
+        }
+    })
+
+    // inserting coutnries into dom
+    reqArr.forEach(ele=>{
+        wrap.innerHTML = ""
+        const box = document.createElement('div')
+        box.classList = "m-5 w-28 h-28 flex justify-center align-middle rounded-lg bg-violet-300 border-red-600 border-4";
+
+        wrap.appendChild(box)
+    })
+        
+    
+})
 
 startWordBtn.addEventListener('click',{
 
@@ -11,9 +45,7 @@ startWordBtn.addEventListener('click',{
 anyWordBtn.addEventListener('click',{
     
 })
-sortBtn.addEventListener('click',{
-    
-})
+
 searchBtn.addEventListener('click',{
     
 })
