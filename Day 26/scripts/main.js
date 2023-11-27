43dc1763be890adde5c30cf4ba71590b6f5aceef
Shivanshu <ks7876555@gmail.com>
2023-11-27 21:09:57 +0530

    Rewrite the code to remove the unused code

--- Day 26/scripts/main.js
+++ Day 26/scripts/main.js
@@ -1,51 +1,24 @@
-// btns
-const startWordBtn = document.querySelector('#stWordBtn')
-const anyWordBtn = document.querySelector('#anyWordBtn')
-const sortBtn = document.querySelector('#sortBtn')
-const searchField = document.querySelector('#searchInput')
-const searchBtn = document.querySelector('#searchBtn')
-const lowerCountries = countries.map(ele => ele.toLowerCase())
-
-//  wraper 
-const wrap = document.getElementById('wrapper')
-
-let reqArr = [] // required countreis
-searchField.addEventListener('input', e=>{
-    let input = ''; input = e.target.value.toLowerCase() // input string
+const search = document.querySelector('#searchInput');
+let countriesLower = countries.map(ele=> ele.toLowerCase())
+let reqArr = []
+search.addEventListener('input', e=>{
     reqArr = []
-    // search with any word
-    // lowerCountries.forEach(ele=> {
-    //     if (ele.includes(input)) {
-    //     reqArr.push(ele.toUpperCase())
-    //     }
-    // })
-
-    // search with starting word
-    lowerCountries.forEach(ele=> {
-        if (ele.startsWith(input)) {
-        reqArr.push(ele.toUpperCase())
+    // search with first word
+    countriesLower.forEach(ele=>{
+        if ( ele.startsWith(e.target.value)){
+            reqArr.push(ele)
         }
     })
+    // // search with any word
+    // countriesLower.forEach(ele=>{
+    //     if ( ele.includes(e.target.value)){
+    //         reqArr.push(ele)
+    //     }
+    // })
+    // a - z default
+    reqArr.sort()
+    // z -a sort 
 
-    // inserting coutnries into dom
-    reqArr.forEach(ele=>{
-        wrap.innerHTML = ""
-        const box = document.createElement('div')
-        box.classList = "m-5 w-28 h-28 flex justify-center align-middle rounded-lg bg-violet-300 border-red-600 border-4";
-
-        wrap.appendChild(box)
-    })
-        
     
-})
-
-startWordBtn.addEventListener('click',{
-
-})
-anyWordBtn.addEventListener('click',{
-    
-})
-
-searchBtn.addEventListener('click',{
-    
+    console.log(reqArr)
 })
