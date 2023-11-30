8cd4578475c1ad380fb2e966c9d9d699c20a676a
Shivanshu <ks7876555@gmail.com>
2023-11-30 22:17:25 +0530

    Add countries box and content

--- Day 26/scripts/main.js
+++ Day 26/scripts/main.js
@@ -1,11 +1,13 @@
 const search = document.querySelector('#searchInput');
 let countriesLower = countries.map(ele=> ele.toLowerCase())
+const wrap = document.querySelector('#wrapper')
+
 let reqArr = []
 search.addEventListener('input', e=>{
     reqArr = []
     // search with first word
     countriesLower.forEach(ele=>{
-        if ( ele.startsWith(e.target.value)){
+        if ( ele.startsWith(e.target.value.toLowerCase())){
             reqArr.push(ele)
         }
     })
@@ -19,6 +21,19 @@ search.addEventListener('input', e=>{
     reqArr.sort()
     // z -a sort 
 
+    wrap.innerHTML = ''
+    reqArr.forEach(ele=>{
+        
+        let wrapper = document.createElement('div')
+        let box = document.createElement('div')
+
+        wrapper.classList = "m-5 w-28 h-28 flex justify-center align-middle rounded-lg bg-slate-600 text-slate-200 border-slate-300 border-4"
+        box.classList = "flex place-self-center text-center"
+        
+        box.textContent = `${ele.toUpperCase()}`
+
+        wrapper.appendChild(box)
+        wrap.appendChild(wrapper)
+    })
     
-    console.log(reqArr)
 })
